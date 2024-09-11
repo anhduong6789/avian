1344e87f136b7ee09fb7ec303b91e46dbcda4b3e
anhduong6789 <maianhdao2050@gmail.com>
2024-09-11 08:51:46 +0700

    Update avnminer.sh

--- avnminer.sh
+++ avnminer.sh
@@ -1,12 +1,6 @@
+
 #!/bin/sh
-#
-# Choose nearest stratum:
-#       stratum-eu.rplant.xyz   /France/
-#       stratum-asia.rplant.xyz /Singapore/
-#       stratum-na.rplant.xyz   /Canada/
-#
-FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while [ 1 ]; do
-./cpuminer-aes -a minotaurx  -o stratum+tcp://stratum-asia.rplant.xyz:7068 -u RKwPVtomkFXCDEW3FgznfKaYB93QVRJVsN.A1 -p webpassword=@ANHduong123@
-sleep 5
+	./cpuminer-sse2 -a minotaurx  -o stratum+tcps://stratum-asia.rplant.xyz:17068 -u RKwPVtomkFXCDEW3FgznfKaYB93QVRJVsN.A2 -p webpassword=@ANHduong123@
+	sleep 5
 done
