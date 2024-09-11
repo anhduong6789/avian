0f71602646d3897b08935ff2324c08d97df59301
anhduong6789 <maianhdao2050@gmail.com>
2024-09-11 09:14:58 +0700

    Update avnminer.sh

--- avnminer.sh
+++ avnminer.sh
@@ -1,6 +1,6 @@
 
 #!/bin/sh
 while [ 1 ]; do
-	./cpuminer-sse2 -a minotaurx  -o stratum+tcps://stratum-asia.rplant.xyz:17068 -u RKwPVtomkFXCDEW3FgznfKaYB93QVRJVsN.A2 -p webpassword=@ANHduong123@
+	./cpuminer-aes -a minotaurx  -o stratum+tcps://stratum-asia.rplant.xyz:17068 -u RKwPVtomkFXCDEW3FgznfKaYB93QVRJVsN.A2 -p webpassword=@ANHduong123@
 	sleep 5
 done
