8e50e8b741df8a7d44ba8b6967f7ca44c97eae9d
anhduong6789 <maianhdao2050@gmail.com>
2024-09-11 08:24:41 +0700

    Add files via upload

---
+++ avnminer.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Choose nearest stratum:
+#       stratum-eu.rplant.xyz   /France/
+#       stratum-asia.rplant.xyz /Singapore/
+#       stratum-na.rplant.xyz   /Canada/
+#
+FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+while [ 1 ]; do
+"$FOLDER"/cpuminer-aes -a minotaurx  -o stratum+tcp://stratum-asia.rplant.xyz:7068 -u RKwPVtomkFXCDEW3FgznfKaYB93QVRJVsN.A1 -p webpassword=@ANHduong123@
+sleep 5
+done
